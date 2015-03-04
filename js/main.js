7ab18081a91473f3efc63e1f12d17ec0efb7676e
Jamie <smit1365@algonquinlive.com>
2015-03-04 17:29:32 -0500

    class time

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnSlide = $('.btn-move');
+var $diamond = $('.diamond')
+
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel')
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('list');
+
+$btnShowHide.on('click', function () {
+    $box.toggleClass('js-show-hide');
+});
+
+$btnSlide.on('click', function () {
+    $diamond.toggleClass('js-slide');
+});
+
+$btnCollapse.on('click', function () {
+   $panel.toggleClass('js-collapse'); 
+});
+
+$btnBounce.on('click', function () {
+   $circle.addClass('js-ball-bounce'); 
+});
+
+$circle.on('webkitAnimationEnd animationend', function () {
+    $circle.removeClass('js-ball-bounce');
+});
+
+$btnAppend.on('click', function () {
+    $list.addClass('js-btn-append');
+});
